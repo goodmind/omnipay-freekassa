5257be34dc04778c2a4003def4b46a5c6b8d1962
andretshurotshka <andwebar@gmail.com>
2017-06-09 12:02:31 +0500

    Create Gateway.php

--- src/Gateway.php
+++ src/Gateway.php
@@ -31,9 +31,10 @@ class Gateway extends AbstractGateway
     public function getDefaultParameters()
     {
         return [
-            'purse' => '',
-            'secretKey'     => '',
-            'testMode'      => false,
+            'purse'      => '',
+            'secretKey'  => '',
+	    'secretKey2' => '',
+            'testMode'   => false,
         ];
     }
 
@@ -73,6 +74,25 @@ class Gateway extends AbstractGateway
     public function setSecretKey($value)
     {
         return $this->setParameter('secretKey', $value);
+    }
+	
+    /**
+     * Get the unified secret key.
+     * @return string secret key
+     */
+    public function getSecretKey2()
+    {
+        return $this->getParameter('secretKey2');
+    }
+
+    /**
+     * Set the unified secret key.
+     * @param string $value secret key
+     * @return self
+     */
+    public function setSecretKey2($value)
+    {
+        return $this->setParameter('secretKey2', $value);
     }
 
     /**
